c53c89631b3998bc916c08a918f3d76c345e0bfe
Haito <hajtosek@gmail.com>
2015-02-11 10:04:17 +0100

    Niebezpieczne grzebanie w refleksjach.

--- app/src/main/java/com/lanceit/haito/lanceit/utils/NetworkUtilities.java
+++ app/src/main/java/com/lanceit/haito/lanceit/utils/NetworkUtilities.java
@@ -1,8 +1,13 @@
 package com.lanceit.haito.lanceit.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.telephony.TelephonyManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class NetworkUtilities {
     public static void callSomeone(Activity activity,long number){
@@ -12,5 +17,24 @@ public class NetworkUtilities {
     }
     public static void callSomeone(Activity activity,String number){
         callSomeone(activity,Long.valueOf(number));
+    }
+    public static void endCall(Context context){
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(context.TELEPHONY_SERVICE);
+
+        Method m1 = null;
+        try {
+            m1 = tm.getClass().getDeclaredMethod("getITelephony");
+            m1.setAccessible(true);
+            Object iTelephony = m1.invoke(tm);
+
+            Method m2 = iTelephony.getClass().getDeclaredMethod("silenceRinger");
+            Method m3 = iTelephony.getClass().getDeclaredMethod("endCall");
+
+            m2.invoke(iTelephony);
+            m3.invoke(iTelephony);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
     }
 }
